a94017e15e7eaf1adcfa67b0cb4f35057764d721
jamesreinke <james@rtsystems.io>
2015-06-16 12:17:32 -0700

    ~

--- app/controllers/Node.scala
+++ app/controllers/Node.scala
@@ -3,8 +3,20 @@ package controllers
 /* Used for building navigation menus */
 object Node {
 
-	sealed abstract class Node
-	case class Leaf(tag: String, link: String) extends Node 
-	case class Tree(tag: String, nodes: List[Node]) extends Node
+	abstract class Menu
+	case class Link(tag: String, link: play.api.mvc.Call, image: String = "") extends Menu
+	case class LinkedLinks(tag: String, image: String = "", links: List[Link]) extends Menu
+
+	/* An example of a profile navigation */
+	val profile = List(
+		new Link("Profile", routes.Application.index(), "icon-user"),
+		new Link("Logout", routes.Authentication.logout(), "icon-key"))
+
+	/* An example of a sidebar navigation */
+	val sidebar = List[Menu](
+		new Link("Dashboard", routes.Application.index(), "icon-home"),
+		new LinkedLinks("See More", "icon-pointer", List(
+			new Link("This is more", routes.Application.index(), "icon-home"),
+			new Link("This is also more", routes.Application.index(), "icon-home"))))
 
 }
