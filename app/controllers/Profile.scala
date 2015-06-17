d91a2d6fe9a842668426b758bc613c280f5a2eca
jamesreinke <james@rtsystems.io>
2015-06-17 14:15:05 -0700

    ~

--- app/controllers/Profile.scala
+++ app/controllers/Profile.scala
@@ -8,12 +8,12 @@ import Node._
 
 object Profile extends Controller {
 
-	val iLink = new Link("Profile", "icon-user", routes.Profile.index())
+	val iLink = new Link("Profile", "fa fa-user", routes.Profile.index())
 	def index = Action {
 		implicit request => Ok(views.html.pages.temp.core(todo.render()))
 	}
 	/* User statistics */
-	val sLink = new Link("Statistics", "icon-calc", routes.Profile.stats())
+	val sLink = new Link("Statistics", "fa fa-calculator", routes.Profile.stats())
 	def stats = Action {
 		implicit request => Ok(views.html.pages.temp.core(todo.render()))
 	}
